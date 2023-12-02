ba532bb5e830557ba6a94b8f087a0dfc4fb32c6b
Svyatoslav Ryazanov <144775578+Rsv567@users.noreply.github.com>
2023-12-02 16:49:00 +0300

    Update form.py

--- myblog/blog/form.py
+++ myblog/blog/form.py
@@ -1,6 +1,9 @@
-from django import  forms
+from django import forms
+
 from .models import Comments
+
+
 class CommentsForm(forms.ModelForm):
     class Meta:
         model = Comments
-        fields = ('name', 'email', 'text_comments')
+        fields = ("name", "email", "text_comments")
