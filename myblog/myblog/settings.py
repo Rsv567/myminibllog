187a6d041bcd7b5ad626d99b299f801d9e39a506
Svyatoslav Ryazanov <144775578+Rsv567@users.noreply.github.com>
2023-12-02 16:55:54 +0300

    Update settings.py

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-+-!$s1hun3(uqnp)xq)c#k1^p63lz#ad^c(n24mby!u36a_t6r"
-
+TOKEN = os.getenv("TOKEN")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
